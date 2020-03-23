d3a31e656d07b44ea5bf9a5eefad7c1cc5ec04b4
KeiferJu <jkf19980216@qq.com>
2020-03-23 18:47:55 +0800

    添加自动部署功能

---
+++ README.md
@@ -0,0 +1,56 @@
+基于`bgwd666`的[deploy](https://github.com/bgwd666/deploy)发布脚本做了一些适合我自己的修改,在此万分感谢.
+
+### 使用方法:
+
+1. 拉取代码
+```
+
+npx degit https://github.com/KeiferJu/auto-deploy.git deploy
+
+cd deploy
+
+npm install
+```
+
+2. 配置使用
+
+在项目package.json中配置命令:
+```
+   ...
+
+  "scripts": {
+      ...
+    "deploy": "node ./deploy/upload.js"
+  },
+
+  ...
+```
+
+然后在`deploy/config.js`里面配置上自己服务器信息,执行命令就可以部署了:
+```
+npm run deploy
+```
+
+### 相关配置项
+
+- CLEAR_OLDFILES: 
+    
+    是否默认删除上传文件夹内容
+- OUTPUT_PATH: 
+    
+    需要上传文件夹路径,默认dist
+- SERVER_PATH: 
+    
+    服务器路径
+- SSH_USER: 
+    
+    服务器用户名
+- PRIVATE_KEY: 
+    
+    用秘钥登录服务器(推荐)的秘钥地址, 本机私钥文件地址(需要在服务器用户目录 一般是 /root/.ssh/authorized_keys 配置公钥 并该文件权限为 600, (.ssh文件夹一般默认隐藏)
+- PASSWORD: 
+    
+    如果密码登录,填入密码,并且不能设置PRIVATE_KEY参数
+- RENAME : 
+    
+    是否改名,是一个对象,参数为OLD_NAME和NEW_NAME
--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 module.exports = Object.freeze({
   development: {//测试
-    SERVER_PATH: '172.17.60.20', // ssh地址 服务器地址
+    SERVER_PATH: 'xxx', // ssh地址 服务器地址
     SSH_USER: 'root', // ssh 用户名 
     //方式一 用秘钥登录服务器(推荐), private 本机私钥文件地址(需要在服务器用户目录 一般是 /root/.ssh/authorized_keys 配置公钥 并该文件权限为 600, (.ssh文件夹一般默认隐藏)
     // PRIVATE_KEY: 'C:/Users/Html5/.ssh/id_rsa', 
     PASSWORD: '123456', //方式二 用密码连接服务器
     PATH: '/var/local', // 需要上传的服务器目录地址 如 /usr/local/nginx/html
-    OUTPUT_PATH: 'docs/__sapper__/export/smx-svelma' // 需要上传文件夹路径,默认dist
+    OUTPUT_PATH: 'dist' // 需要上传文件夹路径,默认dist
   },
   production: {//正式
-    SERVER_PATH: '172.17.60.20', 
+    SERVER_PATH: 'xxx', 
     SSH_USER: 'root',
      //方式一 用秘钥登录服务器(推荐), private 本机私钥文件地址(需要在服务器用户目录 一般是 /root/.ssh/authorized_keys 配置公钥 并该文件权限为 600, (.ssh文件夹一般默认隐藏)
     // PRIVATE_KEY: 'C:/Users/Html5/.ssh/id_rsa', 
-    PASSWORD: '123456', 
+    PASSWORD: 'xxx', 
     PATH: '/var/local', // 需要上传的服务器目录地址 如 /usr/local/nginx/html
-    OUTPUT_PATH: 'docs/__sapper__/export/smx-svelma'  // 需要上传文件夹路径,默认dist
+    OUTPUT_PATH: 'dist'  // 需要上传文件夹路径,默认dist
   }
 })
 
